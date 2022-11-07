f9dd9a11168f937768da003a91187935f3b9b6da
John Chisowa <114460505+johnc49@users.noreply.github.com>
2022-11-07 10:42:04 +0200

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,29 @@
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 5000);
+
+const sidebarOpen = document.querySelector(".sidebarOpen");
+const sidebarClose = document.querySelector(".sidebarClose");
+const nav = document.querySelector("nav");
+const body = document.querySelector("body");
+
+//js code to toggle side bar
+sidebarOpen.addEventListener("click", () => {
+  nav.classList.add("active");
+});
+
+body.addEventListener("click", (e) => {
+  let clickedElm = e.target;
+
+  if (
+    !clickedElm.classList.contains("sidebarOpen") &&
+    !clickedElm.classList.contains("menu")
+  ) {
+    nav.classList.remove("active");
+  }
+});
